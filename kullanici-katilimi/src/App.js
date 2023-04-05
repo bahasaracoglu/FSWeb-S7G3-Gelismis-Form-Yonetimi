8f08ee34036a40fb660b09903b95ee084ccf0229
bahasaracoglu <bahasaracoglu@gmail.com>
2023-04-05 12:54:57 +0300

    kurulum yapıldı.

---
+++ kullanici-katilimi/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Form from "./Form";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
